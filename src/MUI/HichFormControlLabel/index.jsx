4d5f5e2ad4f20ccc86340fc6c0e8b6a76bed0e64
enigma01Z313 <f.ahmadyf94@gmail.com>
2024-06-26 21:21:36 +0330

    add switch component

---
+++ src/MUI/HichFormControlLabel/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function FormControlLabel() {
+  return <div>FormControlLabel</div>
+}
--- src/MUI/HichSwitch/index.jsx
+++ src/MUI/HichSwitch/index.jsx
@@ -5,9 +5,11 @@ import styles from './style.module.css'
 
 export default function HichSwitch({ className, checked, ...rest }) {
   const myClass = `${styles['hich-mui-switch']} 
-    ${checked ? styles['hich-mui-switch-checked'] : ''}
-    ${checked ? 'wwwwwwwwwwwwwwww' : 'sssssssssssssss'}`
+    ${checked ? styles['hich-mui-switch-checked'] : ''}`
   const classes = `${className} ${myClass}`
+
+  console.log('1-----------------------')
+  console.log(rest)
 
   return <Switch className={classes} checked={checked} {...rest} />
 }
