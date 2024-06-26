bf84e765e0f3b79549c4191b00eae9b248a205d9
enigma01Z313 <f.ahmadyf94@gmail.com>
2024-06-26 21:27:17 +0330

    add switch component

--- src/MUI/HichSwitch/index.jsx
+++ src/MUI/HichSwitch/index.jsx
@@ -3,13 +3,11 @@ import React from 'react'
 import { Switch } from '@mui/material'
 import styles from './style.module.css'
 
-export default function HichSwitch({ className, checked, ...rest }) {
+export default function HichSwitch({ className, checked, disabled, ...rest }) {
   const myClass = `${styles['hich-mui-switch']} 
-    ${checked ? styles['hich-mui-switch-checked'] : ''}`
+  ${checked ? styles['hich-mui-switch-checked'] : ''}
+  ${disabled ? styles['hich-mui-switch-disabled'] : ''}`
   const classes = `${className} ${myClass}`
-
-  console.log('1-----------------------')
-  console.log(rest)
 
   return <Switch className={classes} checked={checked} {...rest} />
 }
